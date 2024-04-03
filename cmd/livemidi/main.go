16bc79c2e11822e32757a90217871e375e93aeca
Milos Zivlak <zivlakmilos@gmail.com>
2024-04-03 04:38:01 +0200

    Remove fyne

--- cmd/livemidi/main.go
+++ cmd/livemidi/main.go
@@ -1,15 +1,4 @@
 package main
 
-import (
-	"fyne.io/fyne/v2/app"
-	"github.com/zivlakmilos/live-midi/internal/gui"
-)
-
 func main() {
-	a := app.New()
-
-	w := gui.NewMainWindow(a)
-	w.Show()
-
-	a.Run()
 }
